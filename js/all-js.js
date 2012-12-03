9e7c943fb6b2756a55f4a77040ebca7faa76537c
Abhishek Kumar <abhishek.k@directi.com>
2012-12-03 08:33:34 +0530

    added daily/monthly/yearly charting of errors and better ui

--- js/all-js.js
+++ js/all-js.js
@@ -32,31 +32,57 @@ $('#query-form').submit(function(e) {
 	});
 });	
 
+function get_daily_error_result(day) {
+	var error_name = $("#chart_div_monthly").val().err;
+	day = day.replace("-0","-");
+	var start_date = new Date(day);
+	var start_time_mins = Math.floor((new Date() - new Date(day))/60000);
+	var stop_time_mins = start_time_mins > 1440 ? start_time_mins - 1440 : 0;
+		$.ajax({
+		url : "/backend/get_error_details",
+		data : "row_key="+error_name+"&start_time=" + start_time_mins + "&stop_time="+stop_time_mins,
+		dataType : "json",
+		beforeSend: function() {
+			pre_query();
+		},
+		error: function() {
+			post_error();
+			// enable_query_button();
+		},
+		success: function(response) {
+			post_query_result(response);
+		}
+	});
+}
+
 function make_counter_query() {
+	$("#chart_div").val({type:"yearly"})
 	$.ajax({
 		url : "/backend/all_error_details",
-		data : "type=" + current_loaded_type,
+		data : "type=yearly",
 		dataType : "json",
 		success : function(response) {
 			draw_basic_chart(response);
 			// populate the google chart
 		},
-		error : function(response) {
+		error : function(error) {
+			$("#chart_div").append("Error : " + error.statusText);
 			// notify the user
 		}
 	});
 }
 
-function make_query() {
-	var next = all_types[(all_types.indexOf(current_loaded_type)) + 1];
-	var error_name = $("#chart_div").val();
-	if(next) {
+function make_query(duration_type) {
+	var error_name = $("#chart_div_monthly").val().err;
+
+	// get the current selected division-type ie "yearly, monthly, daily"
+
+	if(current_selected_type) {
 		$.ajax({
 			url : "/backend/get_counter",
-			data : "row_key="+error_name + "&type=" + next + "&cf=" + current_type_value,
+			data : "row_key="+error_name + "&type=" + current_selected_type + "&cf=" + duration_type,
 			dataType : "json",
 			success : function(response) {
-				current_loaded_type = next;
 		        draw_chart(response);
 			},
 			error : function(response) {
--- js/google-charts.js
+++ js/google-charts.js
@@ -1,16 +1,11 @@
 google.load('visualization', '1.0', {'packages':['corechart']});
 google.setOnLoadCallback(make_counter_query);
 
-var all_types = ["yearly", "monthly", "daily"];
-var current_loaded_type = "yearly";
-var current_type_value;
-var current_error;
+var current_selected_type;
 
 function draw_basic_chart(response) {
     var year = new Date().getFullYear();
-    current_type_value = year;
     
-    // Create the data table.
     var data = new google.visualization.DataTable();
     data.addColumn('string', 'Error-Type');
     data.addColumn('number', 'Count');
@@ -33,8 +28,10 @@ function draw_basic_chart(response) {
       var selectedItem = chart.getSelection()[0];
       if (selectedItem) {
         var error_name = data.getValue(selectedItem.row, 0);
-        $("#chart_div").val(error_name);
-        make_query();
+        $("#chart_div_monthly").val({err : error_name, type : "monthly"});
+        $("#chart_div_daily").empty();
+        current_selected_type="monthly";
+        make_query(year);
       }
     }
 
@@ -42,8 +39,7 @@ function draw_basic_chart(response) {
     chart.draw(data, options);
 }
 
-function draw_chart(response, error_name) {
-    current_error = error_name;
+function draw_chart(response) {
     // Create the data table.
     var data = new google.visualization.DataTable();
     data.addColumn('string', 'Division-Type');
@@ -58,24 +54,26 @@ function draw_chart(response, error_name) {
 
     // Set chart options
     var options = {
-        'title':'Count Errors : ' + $("#chart_div").val(),
+        'title':'Count Errors : ' + $("#chart_div_monthly").val().err,
         'width':400,
         'height':300
     };
 
     // Instantiate and draw our chart, passing in some options.
-    var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+    var chart = new google.visualization.PieChart(document.getElementById('chart_div_' + current_selected_type));
 
     google.visualization.events.addListener(chart, 'select', selectHandler);    
     chart.draw(data, options);
 
     function selectHandler() {
-    // current_type_value : depends on what got selected
+        current_selected_type = "daily"    
         var selectedItem = chart.getSelection()[0];
         if (selectedItem) {
             var duration_type = data.getValue(selectedItem.row, 0);
-            current_type_value = duration_type;
-            make_query(current_error);
+            if(duration_type.split("-").length == 2)
+                make_query(duration_type);
+            else
+                get_daily_error_result(duration_type);
         }
     }
 
